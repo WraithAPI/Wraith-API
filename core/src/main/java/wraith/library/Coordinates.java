f1dfd4b6a448050c80e12f1e8678e98c1ad8049a
Daniel Mills <danielmillst@gmail.com>
2017-03-07 01:33:31 -0500

    Update coordinates

--- core/src/main/java/wraith/library/Coordinates.java
+++ core/src/main/java/wraith/library/Coordinates.java
@@ -7,6 +7,8 @@ public enum Coordinates
 {
 	WRAITH_CORE("wraith", "core", GLOBAL_VERSION, REPO_VOLMIT),
 	WRAITH_LANG("wraith", "lang", GLOBAL_VERSION, REPO_VOLMIT),
+	WRAITH_MATH("wraith", "math", GLOBAL_VERSION, REPO_VOLMIT),
+	WRAITH_CONCURRENT("wraith", "concurrent", GLOBAL_VERSION, REPO_VOLMIT),
 	COMMON_LANG("org.apache.commons", "commons-lang3", "3.5", REPO_CENTRAL),
 	COMMON_IO("org.apache.commons", "commons-io", "1.3.2", REPO_CENTRAL),
 	COMMON_MATH("org.apache.commons", "commons-math3", "3.5", REPO_CENTRAL),
