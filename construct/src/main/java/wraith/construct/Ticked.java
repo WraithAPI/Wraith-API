9f1cb7acb5839a97a110c460e3faf5fd4585aa33
Daniel Mills <danielmillst@gmail.com>
2017-03-08 03:44:18 -0500

    Ticked annotation

---
+++ construct/src/main/java/wraith/construct/Ticked.java
@@ -0,0 +1,13 @@
+package wraith.construct;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.*;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target(TYPE)
+public @interface Ticked
+{
+	int value() default 0;
+}
