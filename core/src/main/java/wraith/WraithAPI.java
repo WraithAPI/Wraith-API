3c3a8d3df14b55ce57b67bd378729b440745b97a
Daniel Mills <danielmillst@gmail.com>
2017-03-08 09:37:03 -0500

    Fixc

--- core/src/main/java/wraith/WraithAPI.java
+++ core/src/main/java/wraith/WraithAPI.java
@@ -6,7 +6,7 @@ import wraith.library.LibraryInstaller;
 
 public class WraithAPI
 {
-	public static final String GLOBAL_VERSION = "1.0";
+	public static final String GLOBAL_VERSION = "${project.version}";
 	public static final String REPO_CENTRAL = "https://repo1.maven.org/maven2/";
 	public static final String REPO_VOLMIT = "http://get.volmit.com/";
 	public static final File REPO_LOCAL = new File(new File("wraith"), "repository");
